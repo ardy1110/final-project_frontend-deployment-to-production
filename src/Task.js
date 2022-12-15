4a6fbf1c61878db4163a53a0b371e7742ab56b38
ardy1110 <ardia0908@gmail.com>
2022-12-15 23:45:21 +0700

    second commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://final-project-ardy.web.app" // TODO: replace this
 const studentName = "ARDIANSYAH" // TODO: replace this
 const studentId = "FE2796383" // TODO: replace this
 
