e77b0163a838bbbbf3d32a959295a0435b0adb0d
ardy1110 <ardia0908@gmail.com>
2022-12-15 23:14:41 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "ARDIANSYAH" // TODO: replace this
+const studentId = "FE2796383" // TODO: replace this
+
+export { url, studentName, studentId }
